896a3c1f6f40907c967e980c1c011c5fa9646171
gsusgit <fernandezjodar.jesus@gmail.com>
2024-10-05 08:44:36 +0200

    added sound effects

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const attackvalue = (min, max) => {
 Vue.createApp({
     data() {
         return {
+            gameStarted: false,
             playerHealth: 100,
             monsterHealth: 100,
             PLAYER_MIN_DAMAGE: 5,
@@ -17,7 +18,15 @@ Vue.createApp({
             specialAttackAvailable: false,
             currentRound: 0,
             gameEnded: false,
-            winner: ''
+            winner: '',
+            sounds: {
+                theme: new Audio('/assets/sounds/theme.mp3'),
+                attack: new Audio('/assets/sounds/attack.mp3'),
+                special: new Audio('/assets/sounds/special.mp3'),
+                healing: new Audio('/assets/sounds/healing.mp3'),
+                win: new Audio('/assets/sounds/win.mp3'),
+                loose: new Audio('/assets/sounds/loose.mp3'),
+            }
         }
     },
     computed: {
@@ -26,6 +35,8 @@ Vue.createApp({
                 this.monsterHealth = 0
                 this.gameEnded = true
                 this.winner = 'PLAYER'
+                this.stopSound('theme')
+                this.playSound('win')
             }
             return { width: this.monsterHealth + '%'}
         },
@@ -34,17 +45,24 @@ Vue.createApp({
                 this.playerHealth = 0
                 this.gameEnded = true
                 this.winner = 'MONSTER'
+                this.stopSound('theme')
+                this.playSound('loose')
             } else if (this.playerHealth >= 100)
                 this.playerHealth = 100
             return { width: this.playerHealth + '%'}
         }
     },
     methods: {
+        start() {
+            this.gameStarted = true
+            this.playSound('theme')
+        },
         attackMonster() {
             this.monsterHealth -= attackvalue(this.PLAYER_MIN_DAMAGE, this.PLAYER_MAX_DAMAGE)
             this.attackPlayer()
             this.setRound()
             this.logs.push(`ATTACKING!`)
+            this.playSound('attack')
         },
         attackPlayer() {
             this.playerHealth -= attackvalue(this.MONSTER_MIN_DAMAGE, this.MONSTER_MAX_DAMAGE)
@@ -56,6 +74,7 @@ Vue.createApp({
             this.currentRound = 0
             this.specialAttackAvailable = false
             this.healAvailable = false
+            this.playSound('special')
         },
         setRound() {
             if (this.currentRound < 2) {
@@ -71,9 +90,12 @@ Vue.createApp({
             this.specialAttackAvailable = false
             this.currentRound = 0
             this.logs.push(`HEALING...`)
+            this.playSound('healing')
         },
         surrender() {
             this.playerHealth = 0
+            this.stopSound('theme')
+            this.playSound('loose')
         },
         restart() {
             this.playerHealth = 100
@@ -83,6 +105,18 @@ Vue.createApp({
             this.logs = []
             this.gameEnded = false
             this.winner = ''
+            this.playSound('theme')
+        },
+        playSound(soundName) {
+            if (this.sounds[soundName]) {
+                this.sounds[soundName].play();
+            }
+        },
+        stopSound(soundName) {
+            if (this.sounds[soundName]) {
+                this.sounds[soundName].pause();
+                this.sounds[soundName].currentTime = 0;
+            }
         }
     }
 }).mount('#game')
